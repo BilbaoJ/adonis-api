4f400f8fb8135d50b259b1e0740b1cfa67520576
BilbaoJ <jessicabilbao02@gmail.com>
2025-02-21 16:10:02 -0500

    Adding authentication flow

--- start/routes.ts
+++ start/routes.ts
@@ -7,10 +7,24 @@
 |
 */
 
+const LoginController = () => import('#controllers/auth/login_controller')
+const AuthController = () => import('#controllers/auth_controller')
 import router from '@adonisjs/core/services/router'
+import { middleware } from './kernel.js'
 
 router.get('/', async () => {
   return {
     hello: 'world',
   }
 })
+router
+  .group(() => {
+    router.post('/register', [AuthController, 'register']).as('auth.register')
+
+    router.post('/login', [AuthController, 'login']).as('auth.login')
+
+    router.post('/logout', [AuthController, 'logout']).as('auth.logout').use(middleware.auth())
+
+    router.get('/me', [AuthController, 'me']).as('auth.me')
+  })
+  .prefix('/auth')
